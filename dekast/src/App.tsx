7d132e1578fc4e0061ab739b81283d5e515e852f
Rick Rietbergen <rbps3.2016@gmail.com>
2023-09-15 21:35:53 +0200

    update front

--- dekast/src/App.tsx
+++ dekast/src/App.tsx
@@ -1,8 +1,52 @@
 import { useState } from 'react'
+import { API_URL } from "./constants/links";
 import './App.css'
 
 function App() {
-  const [klantnummer, setKlantnummer] = useState("")
+  const [klantnummer, setKlantnummer] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const handleReport = () => {
+    fetch(`${API_URL}Entree/Aanmelden`, {
+      method: "POST",
+      body: JSON.stringify({
+        klantnummer,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        return new Promise((resolve) => {
+          if (response.ok) {
+            response.json().then((json) =>
+              resolve({
+                status: response.status,
+                ok: response.ok,
+                json,
+              })
+            );
+          } else {
+            resolve({
+              status: response.status,
+              ok: response.ok,
+              json: response,
+            });
+          }
+        });
+      })
+      .then((result) => {
+        const { json, ok } = result as { json: any; ok: any };
+        console.log(json);
+        if (!ok) {
+          setButtonDisabled(false);
+        }
+        if (ok) {
+          setButtonDisabled(true);
+          window.location.href = "/loggedin";
+        }
+      });
+  }
 
   return (
     <>
@@ -17,11 +61,11 @@ function App() {
             </div>
 
             <div className="input_klantnummer">
-              <input className="input" type="text" />
+              <input className="input" type="text" onChange={(e) => {setKlantnummer(e.target.value)}}/>
             </div>
 
             <div className="submit_block">
-              <input className="submit" type="submit" value="Aanmelden"/>
+              <button className="submit" type="submit" onClick={handleReport} disabled={buttonDisabled}>Aanmelden</button>
             </div>
           </div>
         </div>
